d5d8cb9a52d933205eca59dd9bada43866ad18a8
Juzvisen <juzvisen@hotmail.com>
2021-02-11 14:39:51 +0100

    solution

---
+++ Are_you_playing_banjo.py
@@ -0,0 +1,10 @@
+#https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+
+def areYouPlayingBanjo(name):
+    if name[0].lower() == 'r':
+        return f'{name} plays banjo'
+    else:
+        return f'{name} does not play banjo'
+
+
