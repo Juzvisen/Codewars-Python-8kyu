988c66b063c00e2346405a1beb4622ee6d2d3063
Juzvisen <juzvisen@hotmail.com>
2021-02-19 17:46:42 +0100

    solution

--- If_else_syntax_debug.py
+++ If_else_syntax_debug.py
@@ -1,4 +1,10 @@
 # https://www.codewars.com/kata/57089707fe2d01529f00024a
 
 def check_alive(health):
-    return health > 0
+    if health <= 0:
+    	return False
+    else:
+    	return True
+
+
+print(check_alive(0))
