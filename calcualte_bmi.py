6af25b0962f91aa710af7a272f5ff5ae191ef3f0
Juzvisen <juzvisen@hotmail.com>
2021-04-18 18:44:33 +0200

    solution

---
+++ calcualte_bmi.py
@@ -0,0 +1,15 @@
+def bmi(weight, height):
+	bmi = weight/height**2
+	if bmi <= 18.5:
+		return "Underweight"
+	elif bmi <= 25.0:
+		return "Normal"
+	elif bmi <= 30.0:
+	 	return "Overweight"
+	else:
+		return "Obese"
+
+
+
+
+
