92f8277c914770a71bca7cfb304b4a516ea1c14e
Juzvisen <juzvisen@hotmail.com>
2021-05-22 17:59:24 +0200

    solution

---
+++ my_head_is_at_the_wrong_end.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/python
+
+
+def fix_the_meerkat(arr):
+    return arr[::-1]
+
+
---
+++ this_is_odd.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/554003323fd6af1c4200004e/python
+
+
+def is_odd(n):
+    return n % 2 == 1 
+
+
+
