c70e00b7e381f107bd1a667903434582188f2331
Juzvisen <juzvisen@hotmail.com>
2021-05-24 18:06:24 +0200

    solution

---
+++ find_numbers_which_are_divisable_by_given_number.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/55edaba99da3a9c84000003b/train/python
+
+
+def divisable_by(numbers, divisor):
+	return [x for x in numbers if x % divisor == 0]
+
+	
+	
+
+
+
+
+
---
+++ what_is_between.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/python
+
+
+def between(a,b):
+	return list(range(a, b+1))
+	
+	
+
+
+
+
+
