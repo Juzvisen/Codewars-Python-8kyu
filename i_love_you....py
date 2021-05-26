03ac399597e60c78f33e9179b9653a5a646ee271
Juzvisen <juzvisen@hotmail.com>
2021-05-26 12:54:59 +0200

    solution

---
+++ i_love_you....py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/python
+
+def how_much_i_love_you(nb_petals):
+	return ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][nb_petals % 6 - 1]
+
+	
+
+
+
+
+
+
