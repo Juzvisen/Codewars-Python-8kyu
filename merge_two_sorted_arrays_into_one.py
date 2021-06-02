49aaecd1da688f5ebd278505f46a2da876a260ee
Juzvisen <juzvisen@hotmail.com>
2021-06-02 19:12:58 +0200

    solution

---
+++ merge_two_sorted_arrays_into_one.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5899642f6e1b25935d000161/train/python
+
+
+def merge_arrays(arr1, arr2):
+	return sorted(set(arr1 + arr2))
+	
+
+
+print(merge_arrays([1,2,3,4], [5,6,7,8]))
+	
+
