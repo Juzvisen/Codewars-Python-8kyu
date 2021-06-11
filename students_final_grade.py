c83bd5221f42dcf9f16af210174c3e4ed3076e16
Juzvisen <juzvisen@hotmail.com>
2021-06-11 18:43:20 +0200

    solution

---
+++ students_final_grade.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/5ad0d8356165e63c140014d4/train/python
+
+
+def final_grade(exam, projects):
+	if exam > 90 or projects > 10:
+		return 100
+	elif exam > 75 and projects >= 5:
+		return 90
+	elif exam > 50 and projects >= 2:
+		return 75
+	else:
+		return 0
+
