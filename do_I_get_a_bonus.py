fe866ef765a2f71a8542e21bf09d60b9fe8152a5
Juzvisen <juzvisen@hotmail.com>
2021-06-17 20:47:34 +0200

    solution

---
+++ do_I_get_a_bonus.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/python
+
+
+def bonus_time(salary, bonus):
+	if bonus: 
+		return f"${salary * 10}"
+	else: 
+		return f"${salary}"
+
+
