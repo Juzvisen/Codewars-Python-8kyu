ddb389e92a42aa1fe2c5d7a94bacee0ee5ad5784
Juzvisen <juzvisen@hotmail.com>
2021-06-18 21:10:22 +0200

    solution

---
+++ how_do_I_compare_numbers.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/55d8618adfda93c89600012e/train/python
+
+
+def what_is(x):
+	if x == 42:
+		return 'everything'
+	elif x == 42 * 42:
+		return 'everything squared'
+	else:
+		return 'nothing'
+
