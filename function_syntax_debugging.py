54286b69583c1921ee550b16cc2a0120d3029f75
Juzvisen <juzvisen@hotmail.com>
2021-06-25 19:28:05 +0200

    solution

---
+++ function_syntax_debugging.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/python
+
+def main(verb, noun):
+	return verb + noun
+
+
+
+
+
+
---
+++ get_character_from_ASCII_value.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/55ad04714f0b468e8200001c/python
+
+
+def get_char(c):
+	return chr(c)
+
+
+
+
+
+
+
+
