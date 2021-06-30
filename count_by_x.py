fedb1539cac9cdb7d2648a099bea8c6e599483f6
Juzvisen <juzvisen@hotmail.com>
2021-06-30 20:14:19 +0200

    solution

---
+++ count_by_x.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/python
+
+
+def count_by(x,n):
+	return [x * i for i in range(1, n + 1)]
+
+
+
+print(count_by(3,5))
---
+++ for_twins_2_math_operations.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/59c287b16bddd291c700009a/train/python
+
+
+def ice_brick_volume(radius, bottle_length, rim_length):
+	return 2 * radius ** 2 * (bottle_length - rim_length)
+
+
