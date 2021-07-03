4e25dd2ea4c15d32da7bf66d972d8d14fc68fe92
Juzvisen <juzvisen@hotmail.com>
2021-07-03 18:39:02 +0200

    solution

---
+++ floating_point_comparasion.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/5f9f43328a6bff002fa29eb8/python
+
+
+def approx_equals(a,b):
+	return abs(a-b) < 0.001
---
+++ switch_it_up.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/python
+
+
+def switch_it_up(n):
+    return ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'][n]
+
+
+
