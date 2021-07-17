53fa583926473ffc15ffb412e71476269756fc58
Juzvisen <juzvisen@hotmail.com>
2021-07-17 19:45:28 +0200

    solution

---
+++ all_star_code_challange_#18.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/5865918c6b569962950002a1/train/python
+
+
+def str_count(strng, letter):
+	return strng.count(letter)
+
---
+++ number_to_string.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/53934feec44762736c00044b/python
+
+
+a = str(123)
