6ee5a52f873c37f635f3123228fb09e9c1b0ddd3
Juzvisen <juzvisen@hotmail.com>
2021-10-24 15:16:19 +0200

    solution

---
+++ basic_mathematical_operations.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+
+
+def basic_op(operator, value1, value2):
+    if operator == "+":
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "*":
+        return value1 * value2
+    elif operator == "/":
+        return value1 / value2
+
+
