d392c6bdf1897db36d05c67135f6764fd5125863
Juzvisen <juzvisen@hotmail.com>
2021-12-07 20:08:25 +0100

    solution

---
+++ find_the_remainder.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/524f5125ad9c12894e00003f
+
+
+def remainder(a,b):
+	if min(a,b) == 0:
+		return None
+	elif a > b:
+		return a % b
+	else:
+		return b % a 
+
+
+	
+	
+
+
+
+
