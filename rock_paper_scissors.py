81d036dd5f092b36da80867e1b1ea3c740ec6a83
Juzvisen <juzvisen@hotmail.com>
2022-01-05 17:17:20 +0100

    solution

---
+++ rock_paper_scissors.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/5672a98bdbdd995fad00000f/python
+
+
+def rps(p1, p2):
+    beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+    if beats[p1] == p2:
+        return "Player 1 won!"
+    if beats[p2] == p1:
+        return "Player 2 won!"
+    return "Draw!"
